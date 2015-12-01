5248818c6b873110da80651e5666f58d77ab23b0
TwoRice <2rimagowan@gmail.com>
2015-12-01 20:52:20 +0000

    changed to use header file

--- Player.cpp
+++ Player.cpp
@@ -1,40 +1,35 @@
 #include <string>
+#include "Player.h"
 
-class Player {
-private:
-	string name;
-	int chips;
-public:
-	Player(string name) {
-		this->name = name;
-		this->chips = 2000;
-	}
+Player::Player(string name) {
+	this->name = name;
+	this->chips = 2000;
+}
 
-	Player(string name, int chips) {
-		this->name = name;
-		this->chips = chips;
-	}
+Player::Player(string name, int chips) {
+	this->name = name;
+	this->chips = chips;
+}
 
-	// bets <amount> number of chips
-	// if player doesn't have enough chips return false
-	// otherwise deduct chips from the player, and return true
-	bool bet(int amount) {
-		if(amount>chips) {
-			return false;
-		} 
-		chips-=amount;
-		return true;
-	}
+// bets <amount> number of chips
+// if player doesn't have enough chips return false
+// otherwise deduct chips from the player, and return true
+bool Player::bet(int amount) {
+	if(amount>chips) {
+		return false;
+	} 
+	chips-=amount;
+	return true;
+}
 
-	string getName() {
-		return this->name;
-	}
+string Player::getName() {
+	return this->name;
+}
 
-	int getAmount() {
-		return this->chips;
-	}
+int Plyaer::getAmount() {
+	return this->chips;
+}
 
-	void appendName(string end) {
-		this->name+=end;
-	}
+void Player::appendName(string end) {
+	this->name+=end;
 }
