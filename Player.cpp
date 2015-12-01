7c4502aa28799efbe5c2b246da40edc1029c222a
James Neill <tjneill@dundee.ac.uk>
2015-12-01 20:23:36 +0000

    added bet() & getChips()

--- Player.cpp
+++ Player.cpp
@@ -3,10 +3,30 @@
 class Player {
 private:
 	string name;
-	int money;
+	int chips;
 public:
 	Player(string name) {
 		this->name = name;
-		int money = 2000;
+		this->chips = 2000;
+	}
+
+	Player(string name, int chips) {
+		this->name = name;
+		this->chips = chips;
+	}
+
+	// bets <amount> number of chips
+	// if player doesn't have enough chips return false
+	// otherwise deduct chips from the player, and return true
+	bool bet(int amount) {
+		if(amount>chips) {
+			return false;
+		} 
+		chips-=amount;
+		return true;
+	}
+
+	int getChips() {
+		return this->chips;
 	}
 }
