95166fa587e60370beeb63b50ee787a73fe15e69
TwoRice <2rimagowan@gmail.com>
2015-12-01 20:40:20 +0000

    Added Menu

--- Menu.cpp
+++ Menu.cpp
@@ -5,8 +5,52 @@
 
 using namespace std;
 
+Menu::Menu(){
+
+
+}
+
+void Menu::display(){
+
+	cout << "1. Start" << endl;
+	cout << "2. Quit" << endl;
+
+}
+
+void Menu::read(){
+
+	int selection;
+
+	do{
+
+		display();
+		cout << "Enter Selection : " << endl;
+		cin >> selection;
+
+		switch(selection){
+
+			case 1:
+				break;
+
+			case 2:
+			
+				cout << "Exiting Program" << endl;
+				break;
+
+			default:
+			
+				cout << "Invalid Selection" << endl;
+				breeak;		
+
+		}
+
+	}while(selection != 2);
+
+}
+
 int main(){
 
+	read();
 	return 0;
 
 }
