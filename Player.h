bfe829d1fb3ceaba5d270126fb20e496a881b74e
James Neill <tjneill@dundee.ac.uk>
2015-12-01 20:46:02 +0000

    Added prototypes for Player.cpp

---
+++ Player.h
@@ -0,0 +1,15 @@
+#include <string>
+
+class Player {
+private:
+	string name;
+	int chips;
+public:
+	Player(string name);
+	Player(string name, int chips);
+
+	bool bet(int amount);
+	string getName();
+	int getAmount();
+	void appendName(string end);
+}
